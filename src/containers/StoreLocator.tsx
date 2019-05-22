0d28579ff3c0b5408281b0dce68968d435270a03
Vitalie Andries <vitalie.andries@codefactorygroup.com>
2019-05-22 11:08:01 +0300

    migrate a component

--- src/containers/StoreLocator.tsx
+++ src/containers/StoreLocator.tsx
@@ -4,20 +4,31 @@ import Button from '../views/Button';
 import Header from '../views/Header';
 import Map from '../views/Map';
 
-const StoreLocator = (props) => {
+export interface Shop {
+    location: string;
+    imageName: string;
+    address: string;
+}
+
+interface LocatorProps {
+    children?: React.ReactNode;
+    shops?: Shop[]
+}
+
+const StoreLocator: React.FC<LocatorProps> = (props: LocatorProps) => {
     const [currentMap, setCurrentMap] = useState('none.png');
-    const [shops, setShops] = useState(props.shops || []);
+    const [shops, setShops] = useState<Shop[]>(props.shops || []);
 
     useEffect(() => {
         const fetchData = async () => {
-            const result = await axios.get('/data/shops.json');
+            const result = await axios.get<{ shops: Shop[] }>('/data/shops.json');
             setShops(result.data.shops);
         };
 
         fetchData();
     }, []);
 
-    let storeButtons = shops.map(({location, imageName}, index) => (
+    let storeButtons = shops.map(({location, imageName}: Shop, index: number) => (
         <Button
             key={index}
             location={location}
